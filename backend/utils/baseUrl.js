12661f87e6838a277034d3927f92ac554092cc34
Shivam Singh <136624769+shivmSingh1@users.noreply.github.com>
2025-10-24 21:28:08 +0530

    Update FRONTEND_URL to new deployment URL

--- backend/utils/baseUrl.js
+++ backend/utils/baseUrl.js
@@ -1,5 +1,5 @@
 // const FRONTEND_URL = "http://localhost:5173"
 
-const FRONTEND_URL = "https://chatmate1-frontend.onrender.com"
+const FRONTEND_URL = "https://chatmate0-1-1.onrender.com"
 
 module.exports = FRONTEND_URL
